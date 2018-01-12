f3bbd8f3b108b8f28003bad4692223f8f76ef756
wanglei <wanglei@koolyun.com>
2018-01-12 14:37:41 +0800

    init

---
+++ src/main/java/wang/angi/sample/mybatis/generator/maven/App.java
@@ -0,0 +1,13 @@
+package wang.angi.sample.mybatis.generator.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
